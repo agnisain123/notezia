f7491d9ddda1ce47c90a4cbc123a7d9b93479e58
Agni Sain <agnisain123@gmail.com>
2021-12-11 20:01:52 +0530

    Project initialzed

---
+++ notezia/notes/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Note(models.Model):
+    route = models.CharField(max_length = 180)
+    password = models.CharField(max_length= 64)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.route
