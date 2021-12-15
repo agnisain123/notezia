3b3c5f25d65c10c5f14a02ae468b2aef4f02450d
singhayushh <coding.ayush@gmail.com>
2021-12-16 01:03:47 +0530

    frontend setup

--- notezia/notezia/settings.py
+++ notezia/notezia/settings.py
@@ -11,9 +11,11 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATE_DIR = os.path.join(BASE_DIR,"templates")
 
 
 # Quick-start development settings - unsuitable for production
@@ -58,7 +60,7 @@ ROOT_URLCONF = 'notezia.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
